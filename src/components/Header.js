f3bb03f73b56d26c87f88411fe91919c59b8cc30
annaestherrr <annaesther_15@hotmail.com>
2022-07-08 15:03:44 -0300

    ajustando medidas e estilo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
+//Libs
 import React from "react";
 import styled from "styled-components";
+
+//Image
+import LogoVnW from "../assets/LogoVnW.png";
 
 const Container = styled.div`
   width: 100%;
@@ -7,57 +11,68 @@ const Container = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  background-color: #191970;
-  border: red solid;
 `;
 
 const Logo = styled.div`
   width: 10%;
-  display: flex;
-  align-items: flex-start;
-  border: pink solid;
-`;
+  height: 3.5rem;
 
-const Nav = styled.nav`
-  width: 80%;
-  display: flex;
-  border: gray solid;
-`;
+  img {
+    width: 4rem;
+    height: 3rem;
+    position: fixed;
 
-const ListHeader = styled.ul`
- width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  list-style: none;
-  border: yellow solid;
-
-  li{
-    padding-left: 2rem;
-    color: white;
-
-    :hover{
+    :hover {
       cursor: pointer;
     }
   }
 `;
 
+const Nav = styled.nav`
+  width: 90%;
+  display: flex;
+`;
+
+const ListHeader = styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  list-style: none;
+
+  li {
+    font-family: Montserrat, Regular;
+    font-size: 1rem;
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
+    color: white;
+    text-align: center;
+    border: transparent solid 2px;
+    border-radius: 12px;
+
+    :hover {
+      cursor: pointer;
+      border-color: #FFAC2D;
+      transition: all 0.5s;
+    }
+  }
+`;
 
 export default function Header() {
-  return(
-    <Container>
-    <Logo>
-      <p>IMG VainaWeb</p>
-    </Logo>
-    <Nav>
-      <ListHeader>
-        <li>Sobre</li>
-        <li>Formação</li>
-        <li>Equipe</li>
-        <li>Eventos</li>
-        <li>Contatos</li>
-      </ListHeader>
-    </Nav>
+  return (
+    <Container >
+      <Logo>
+        <img src={LogoVnW} alt="Logo Vai na Web" />
+      </Logo>
+      <Nav>
+        <ListHeader>
+          <li>Sobre</li>
+          <li>Formação</li>
+          <li>Equipe</li>
+          <li>Eventos</li>
+          <li>Contatos</li>
+        </ListHeader>
+      </Nav>
     </Container>
-  )
+  );
 }
